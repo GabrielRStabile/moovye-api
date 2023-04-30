2677c01012dd7ab77ac72b14519ce1c2e3f0f4af
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-04-30 16:10:59 -0300

    feat: added migrations to add birth date on users

--- src/Movye.Identity/Migrations/20230430165551_AddedDateOfBirthOnUser.Designer.cs
+++ src/Movye.Identity/Migrations/20230430165551_AddedDateOfBirthOnUser.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movye.Identity.Data;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Movye.Identity.Migrations
 {
     [DbContext(typeof(IdentityDataContext))]
-    partial class IdentityDataContextModelSnapshot : ModelSnapshot
+    [Migration("20230430165551_AddedDateOfBirthOnUser")]
+    partial class AddedDateOfBirthOnUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
