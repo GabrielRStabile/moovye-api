8a079c20213e820c38ec8c510f7672c5240a81c2
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-04-30 16:07:58 -0300

    feat: created jwt service interface and their response

---
+++ src/Movye.Domain/Interfaces/Services/IJwtService/IJwtService.cs
@@ -0,0 +1,12 @@
+using System.Security.Claims;
+using Movye.Domain.Entities;
+using Movye.Domain.Interfaces.Services.IJwtService.Responses;
+
+namespace Movye.Domain.Interfaces.Services.IJwtService
+{
+    public interface IJwtService
+    {
+        Task<JwtServiceGenerateJwtTokenResponse> GenerateJwtToken(User user);
+        Task<IList<Claim>> GetClaims(User user);
+    }
+}
