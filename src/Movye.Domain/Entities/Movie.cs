07714c15357fd54fa982f5548d7e4a2e3d87c374
Gabriel Rocha Stabile <gabrielrostabile@outlook.com>
2023-04-30 16:05:45 -0300

    refactor: added constructor for content interface

--- src/Movye.Domain/Entities/Movie.cs
+++ src/Movye.Domain/Entities/Movie.cs
@@ -2,6 +2,8 @@ namespace Movye.Domain.Entities
 {
     public class Movie : Content
     {
-
+        public Movie(string name) : base(name)
+        {
+        }
     }
 }
--- src/Movye.Domain/Entities/Shared/Content.cs
+++ src/Movye.Domain/Entities/Shared/Content.cs
@@ -4,6 +4,11 @@ namespace Movye.Domain.Entities
 {
     public abstract class Content : Entity
     {
+        protected Content(string name)
+        {
+            Name = name;
+        }
+
         public string Name { get; protected set; }
 
     }
